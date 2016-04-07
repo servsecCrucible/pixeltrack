b3da4593f0aaf5e0fcaffbc7f4dbfadb0035b4ef
Jherez Taylor <jherez.taylor@gmail.com>
2016-04-07 23:58:37 +0800

    API and Class details

--- app.rb
+++ app.rb
@@ -12,5 +12,56 @@ class PixelTrackerApp < Sinatra::Base
   get '/?' do
     'Welcome to Servesec Crucible Pixeltrack service'
   end
-end
 
+  get '/api/v1/?' do
+    # TODO: show all routes as json with links
+  end
+
+  get '/api/v1/pixels/?' do
+    content_type 'application/json'
+    id_list = Pixel.all
+
+    { pixel_id: id_list }.to_json
+  end
+
+  get '/api/v1/pixels/:id.json' do
+    content_type 'application/json'
+
+    begin
+      { pixel: Pixel.find(params[:id]) }.to_json
+    rescue => e
+      status 404
+      logger.info "FAILED to GET pixel: #{e.inspect}"
+    end
+  end
+
+  get '/api/v1/pixels/:id/views' do
+    content_type 'text/plain'
+
+    begin
+      Base64.strict_decode64 Pixel.find(params[:id]).views
+    rescue => e
+      status 404
+      e.inspect
+    end
+  end
+
+  post '/api/v1/pixels/?' do
+    content_type 'application/json'
+
+    begin
+      new_data = JSON.parse(request.body.read)
+      new_config = Pixel.new(new_data)
+      if new_config.save
+        logger.info "NEW PIXEL STORED: #{new_pixel.id}"
+      else
+        halt 400, "Could not store pixel: #{new_pixel}"
+      end
+
+      redirect '/api/v1/pixels/' + new_pixel.id + '.json'
+    rescue => e
+      status 400
+      logger.info "FAILED to create new pixel: #{e.inspect}"
+    end
+  end
+end
