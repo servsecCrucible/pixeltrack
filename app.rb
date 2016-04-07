b86c6c91d85f00fd93a23ed296a47b21f5333d2a
rjollet- <renaudjollet@gmail.com>
2016-04-07 23:42:19 +0800

    API instruction
    the api root, /api/v1/ return the instruction
    of the api in json format
    
    Signed-off-by: rjollet- <renaudjollet@gmail.com>

--- app.rb
+++ app.rb
@@ -12,5 +12,40 @@ class PixelTrackerApp < Sinatra::Base
   get '/?' do
     'Welcome to Servesec Crucible Pixeltrack service'
   end
+
+  get '/api/v1/?' do
+    apidoc = {
+      '/api/v1/' => {
+        'get' => {
+          'parameters' => 'None',
+          'status' => '200',
+          'data' => 'help'
+        }
+      },
+      '/api/v1/pixels/' => {
+        'get' => {
+          'parameters' => 'None',
+          'status' => '200',
+          'data' => 'return the list of pixel ids'
+        },
+        'post' => {
+          'parameters' => {
+            'id' => 'id of the new pixel (optional)',
+            'label' => 'label of the pixel'
+          },
+          'status' => '300',
+          'data' => 'redirect to the new created pixel'
+        }
+      },
+      '/api/v1/pixels/:id.json' => {
+        'get' => {
+          'parameters' => 'None',
+          'status' => '200',
+          'data' => 'pixel details'
+        }
+      }
+    }
+    apidoc.to_json
+  end
 end
 
