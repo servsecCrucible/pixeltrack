7c4423ef540b1d11566ccd34370d758dcb97f572
Jherez Taylor <jherez.taylor@gmail.com>
2016-04-14 17:17:59 +0800

    Added spec files for each table, Campaign and Tracker

--- app.rb
+++ app.rb
@@ -49,53 +49,51 @@ class PixelTrackerApp < Sinatra::Base
     apidoc.to_json
   end
 
-=begin
-  get '/api/v1/pixels/?' do
-    content_type 'application/json'
-    id_list = Pixel.all
-
-    { pixel_id: id_list }.to_json
-  end
-
-  get '/api/v1/pixels/:id.json' do
-    content_type 'application/json'
-
-    begin
-      { pixel: Pixel.find(params[:id]) }.to_json
-    rescue => e
-      status 404
-      logger.info "FAILED to GET pixel: #{e.inspect}"
-    end
-  end
-
-  get '/api/v1/pixels/:id/views' do
-    content_type 'text/plain'
-
-    begin
-      Base64.strict_decode64 Pixel.find(params[:id]).views
-    rescue => e
-      status 404
-      e.inspect
-    end
-  end
-
-  post '/api/v1/pixels/?' do
-    content_type 'application/json'
-
-    begin
-      new_data = JSON.parse(request.body.read)
-      new_pixel = Pixel.new(new_data)
-      if new_pixel.save
-        logger.info "NEW PIXEL STORED: #{new_pixel.id}"
-      else
-        halt 400, "Could not store pixel: #{new_pixel}"
-      end
-
-      redirect '/api/v1/pixels/' + new_pixel.id + '.json'
-    rescue => e
-      status 400
-      logger.info "FAILED to create new pixel: #{e.inspect}"
-    end
-  end
-=end
+  #   get '/api/v1/pixels/?' do
+  #     content_type 'application/json'
+  #     id_list = Pixel.all
+  #
+  #     { pixel_id: id_list }.to_json
+  #   end
+  #
+  #   get '/api/v1/pixels/:id.json' do
+  #     content_type 'application/json'
+  #
+  #     begin
+  #       { pixel: Pixel.find(params[:id]) }.to_json
+  #     rescue => e
+  #       status 404
+  #       logger.info "FAILED to GET pixel: #{e.inspect}"
+  #     end
+  #   end
+  #
+  #   get '/api/v1/pixels/:id/views' do
+  #     content_type 'text/plain'
+  #
+  #     begin
+  #       Base64.strict_decode64 Pixel.find(params[:id]).views
+  #     rescue => e
+  #       status 404
+  #       e.inspect
+  #     end
+  #   end
+  #
+  #   post '/api/v1/pixels/?' do
+  #     content_type 'application/json'
+  #
+  #     begin
+  #       new_data = JSON.parse(request.body.read)
+  #       new_pixel = Pixel.new(new_data)
+  #       if new_pixel.save
+  #         logger.info "NEW PIXEL STORED: #{new_pixel.id}"
+  #       else
+  #         halt 400, "Could not store pixel: #{new_pixel}"
+  #       end
+  #
+  #       redirect '/api/v1/pixels/' + new_pixel.id + '.json'
+  #     rescue => e
+  #       status 400
+  #       logger.info "FAILED to create new pixel: #{e.inspect}"
+  #     end
+  #   end
 end
---
+++ specs/api_spec.rb
@@ -0,0 +1,10 @@
+``
+require_relative './spec_helper'
+
+describe 'Test root route' do
+  it 'should find the root route' do
+    get '/'
+    _(last_response.body).must_include 'Pixeltrack'
+    _(last_response.status).must_equal 200
+  end
+end
