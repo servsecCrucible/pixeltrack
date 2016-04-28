0503cebf4e95dbdfe3f1236197623f9855fb8bcf
Nicholas Danks <nicholasdanks@hotmail.com>
2016-04-28 17:09:54 +0800

    Updated securemodel, migrations and models

--- models/campaign.rb
+++ models/campaign.rb
@@ -3,13 +3,13 @@ require 'sequel'
 
 # Holds the campaigns information for each pixel tracker
 class Campaign < Sequel::Model
-  include EncryptableModel
+  include SecureModel
 
   one_to_many :trackers
   set_allowed_columns :label 
 
   def label=(label_plaintext)
-    self.label_encrypted = encrypt(label_plaintext)
+    self.label_encrypted = encrypt(label_plaintext) if label_plaintext
   end
 
   def label
