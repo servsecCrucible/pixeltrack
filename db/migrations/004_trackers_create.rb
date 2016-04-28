a65aed9c735c690089c69b686e190179e1885e54
Jherez Taylor <jherez.taylor@gmail.com>
2016-04-28 18:35:08 +0800

    updated migrations

--- db/migrations/004_trackers_create.rb
+++ db/migrations/004_trackers_create.rb
@@ -5,9 +5,10 @@ Sequel.migration do
     create_table(:trackers) do
       String :id, type: :uuid, primary_key: true
       foreign_key :campaign_id
-
       String :label_encrypted, text: true, null: false
       String :url, unique: true
+      DateTime :created_at
+      DateTime :updated_at
     end
   end
 end
