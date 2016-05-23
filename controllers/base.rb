81bd9dd6637e85c92c2ddf240ac80a624e113c18
Nicholas Danks <nicholasdanks@hotmail.com>
2016-05-23 13:32:27 +0800

    base.rb

--- controllers/base.rb
+++ controllers/base.rb
@@ -9,6 +9,19 @@ class PixelTrackerAPI < Sinatra::Base
     use Rack::SslEnforcer
   end
 
+  def authenticated_account(env)
+    scheme, auth_token = env['HTTP_AUTHORIZATION'].split(' ')
+    account_payload = JSON.load JWE.decrypt(auth_token)
+    (scheme =~ /^Bearer$/i) ? account_payload : nil
+  end
+  
+  def authorized_account?(env, username)
+    account = authenticated_account(env)
+    account['username'] == username
+  rescue
+    false
+  end
+  
   before do
     host_url = "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
     @request_url = URI.join(host_url, request.path.to_s)
